5d6e101f165146315c6175febd9b661ea4a676cb
Da Hwa <john.213steven@gmail.com>
2024-10-09 23:32:08 +0700

    adjusting

--- webapp/models.py
+++ webapp/models.py
@@ -1,4 +1,4 @@
-from webapp import db, app
+from webapp import db
 from flask_login import UserMixin
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
--- webapp/views.py
+++ webapp/views.py
@@ -4,10 +4,6 @@ from flask import Blueprint, render_template
 views = Blueprint("views", __name__)
 
 
-# @views.route("/")
-# def welcome():
-#     return render_template("welcome.html")
-
 @views.route("/")
 @views.route("/home")
 def home():
